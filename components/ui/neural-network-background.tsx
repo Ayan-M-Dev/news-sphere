ad8b7597fb8a323f6f624681663fc504c3d81ca0
Riki Kashyap <xonic.code@gmail.com>
2025-11-16 19:03:32 +0530

    Fix Shader in neural-network-backgroung.tsx

--- components/ui/neural-network-background.tsx
+++ components/ui/neural-network-background.tsx
@@ -163,7 +163,7 @@ function ShaderPlane() {
   });
 
   return (
-    <mesh ref={meshRef} position={[0, -0.75, -0.5]}>
+    <mesh ref={meshRef} position={[0, -0.75, -0.2]}>
       <planeGeometry args={[4, 4]} />
       <cPPNShaderMaterial ref={materialRef} side={THREE.DoubleSide} />
     </mesh>
